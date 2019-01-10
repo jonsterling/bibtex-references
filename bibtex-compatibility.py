fc8d9e52dec627929eb0d744999233ebd3fadee5
Jonathan Sterling <jon@jonmsterling.com>
2019-01-10 13:31:47 -0500

    scripts to maintain compatibility with bibtex

---
+++ bibtex-compatibility.py
@@ -0,0 +1,34 @@
+import re
+
+month_names = {
+  1: "jan",
+  2: "feb",
+  3: "mar",
+  4: "apr",
+  5: "may",
+  6: "jun",
+  7: "jul",
+  8: "aug",
+  9: "sep",
+  10: "oct",
+  11: "nov",
+  12: "dec"
+}
+
+old_db = open("refs.bib","r")
+new_db = open("refs-bibtex.bib","w")
+for line in old_db.readlines():
+    date_pattern = re.search("date.*{(\d+)-?(\d+)?}",line)
+    if date_pattern:
+        new_db.write("  year = {{{0:s}}},\n".format(date_pattern.group(1)))
+        # print "  year = {{{0:s}}},\n".format(date_pattern.group(1)),
+        if date_pattern.group(2) != None:
+            month = month_names[int(date_pattern.group(2))];
+            new_db.write("  month = {},\n".format(month))
+        continue
+    if re.search("journaltitle",line):
+        new_db.write(line.replace("journaltitle","journal"))
+        continue
+    new_db.write(line)
+old_db.close()
+new_db.close()
